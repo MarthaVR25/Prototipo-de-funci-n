691257d8413e4774500da19acd63da343daa2b7f
Martha <ada_hack279@hackademy.mx>
2022-03-27 00:12:59 -0600

    Agregué el archivo

---
+++ prototipofuncion.py
@@ -0,0 +1,16 @@
+print("Prototipo de función")
+print("Suma de dos números enteros")
+
+def menu():
+	print("[x] Primer número entero")
+	print("[y] Segundo número entero")
+	
+
+menu()
+
+x=int(input("x ="))
+y=int(input("y ="))
+
+sum= x+y
+
+print("La suma de (x + y) es: ", sum)
